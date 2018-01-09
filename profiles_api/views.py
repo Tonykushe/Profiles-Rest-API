4cb9344fbb9cb5e37f089d96f62526247de87493
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-09 23:15:50 +0300

    Extra viewset methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -51,3 +51,34 @@ class EmployeeViewSet(viewsets.ViewSet):
     varemployee = employee.objects.all()
     serializer = employeeSerializer(varemployee, many=True)
     return Response(serializer.data)
+
+  def create(self, request):
+    """ Post new Employees """
+    serializer = employeeSerializer(data=request.data)
+
+    if serializer.is_valid():
+      name = serializer.data.get('firstname')
+      message = 'Hello {0}'.format(name)
+      return Response({'message' : message})
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def retrieve(self, request, pk=None):
+    """ Handles getting an object by it's ID """
+
+    return Response({'http_method': 'GET'})
+
+  def update(self, request, pk=None):
+    """ Handles updating an object """
+
+    return Response({'http_method': 'PUT'})
+
+  def partial_update(self, request, pk=None):
+    """ Handles updating part of an object """
+
+    return Response({'http_method': 'PATCH'})
+
+  def destroy(self, request, pk=None):
+    """ Handles removing an object """
+
+    return Response({'http_method': 'DELETE'})
